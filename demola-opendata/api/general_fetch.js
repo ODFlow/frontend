7b23203b47552c9b287e0160da4f83f00802b91c
dstring <dstring2@proton.me>
2025-03-11 18:43:37 +0200

    Minor changes. Refs #11

---
+++ demola-opendata/api/general_fetch.js
@@ -0,0 +1,66 @@
+"use client";
+import axios from 'axios'
+import { useState } from 'react';
+import { trafficQuery } from './sq-api_table_statfin_ton_pxt_12qh.px';
+import { employmentRate } from './sq-api_table_statfin_tyonv_pxt_12r5.px';
+
+const GeneralFetch = () => {
+const [city, setCity] = useState(null);
+const [error, setError] = useState(null);
+const [response, setResponse] = useState(null);
+
+// const url = 'https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/ton/statfin_ton_pxt_12qh.px'
+const url = 'https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/tyonv/statfin_tyonv_pxt_12r5.px'
+
+
+
+
+
+
+const tryReq = async (event) => {
+    event.preventDefault();
+    try {
+        const re = await axios.post(url, employmentRate.queryObj);
+        setResponse(re.data);
+        console.log(re.data.dimension.Alue.category.label);
+        //console.log(re.data.dimension.Vuosi.category.label);
+        console.log(re.data.dimension.Kuukausi.category.label);
+        console.log(re.data.dimension.Tiedot.category.label);
+        console.log(re.data.value);
+    } catch (e) {
+        setError(e);
+    } finally {}
+}
+
+function parseData(data) {
+    const areas = Object.values(data.dimension.Alue.category.label);
+    const years = Object.values(re.data.dimension.Vuosi.category.label);
+    const accidentTypes = Object.values(data.dimension.Tiedot.category.label);
+
+    const table = [];
+
+    const areaSize = data.size[0];
+    const yearSize = data.size[1];
+    const typeSize = data.size[2];
+
+    const values = [];
+
+    
+}
+
+
+
+return (
+    <div>
+        <button onClick={tryReq}>Button</button>
+        {response && (<pre>{JSON.stringify(response, null, 2)}</pre>)}
+        
+    </div>
+)
+
+}
+
+
+
+
+export default  GeneralFetch;
