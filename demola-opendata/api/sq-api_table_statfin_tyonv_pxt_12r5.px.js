56dbfb0c2a0d41f7d47fe85c365d294564aa14e5
dstring <dstring2@proton.me>
2025-03-11 18:42:48 +0200

    JSON query params for testing. Refs #11

---
+++ demola-opendata/api/sq-api_table_statfin_tyonv_pxt_12r5.px.js
@@ -0,0 +1,48 @@
+export const employmentRate = {"queryObj":{
+  "query": [
+    {
+      "code": "Alue",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "KU481",
+          "KU483",
+          "KU484",
+          "KU489",
+          "KU491",
+          "KU494",
+          "KU495",
+          "KU498"
+        ]
+      }
+    },
+    {
+      "code": "Kuukausi",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "2024M06",
+          "2024M07",
+          "2024M08",
+          "2024M09",
+          "2024M10",
+          "2024M11",
+          "2024M12",
+          "2025M01"
+        ]
+      }
+    },
+    {
+      "code": "Tiedot",
+      "selection": {
+        "filter": "item",
+        "values": [
+          "TYOTOSUUS"
+        ]
+      }
+    }
+  ],
+  "response": {
+    "format": "json-stat2"
+  }
+},"tableIdForQuery":"statfin_tyonv_pxt_12r5.px"}
