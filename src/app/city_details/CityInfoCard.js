7d7a254a620aa29719bd36599a39f0c3410040ac
dstring <dstring2@proton.me>
2025-04-06 14:28:15 +0300

    feat: add loading animation

--- src/app/city_details/CityInfoCard.js
+++ src/app/city_details/CityInfoCard.js
@@ -1,15 +1,16 @@
 // src/components/city/CityInfoCard.jsx
 import { useTheme } from '@mui/material/styles';
 import { Card } from "@/components/ui/card";
-import ApartmentIcon from '@mui/icons-material/Apartment';
+import {useLoading} from "@/app/city_details/LoadingContext";
 
 export default function CityInfoCard({ cityName }) {
     const theme = useTheme();
+    const {loading} = useLoading();
 
     return (
         <Card data-testid="data-card" className="city-card" style={{ background: theme.palette.background.paper }}>
-            <h1 className="city-name">{cityName}</h1>
-            <div className="city-info">
+            <h1 className="city-name">{cityName.toLocaleUpperCase()}</h1>
+            <div className="city-info" style={{ fontFamily: loading ? "Flow Circular, system-ui" : "sans-serif" }}>
                 Located amidst serene landscapes of lakes and forests,
                 this city offers a peaceful and natural environment perfect for outdoor activities year-round.
                 With a mix of modern infrastructure and traditional Finnish architecture,
